75092ef019663749b4beda3d2f54608af384f90a
DrTenma <113189733+Drtenma34@users.noreply.github.com>
2025-06-13 11:38:33 +0200

    initial commit

---
+++ tests/test_driver.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")  # Optionnel, utile si tu veux exécuter sans ouvrir la fenêtre
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://www.google.com")
+print("Titre de la page:", driver.title)
+
+driver.quit()
